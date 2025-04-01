434af234c9159e5a56b33b6748731c3de75cb4d2
tadzio <tadzio.holtrop@icloud.com>
2025-04-01 22:59:54 +0100

    set driver for writing geopandas to disk

--- src/flood_alert/datasets/datasets.py
+++ src/flood_alert/datasets/datasets.py
@@ -1,12 +1,19 @@
+from typing import Literal
+
 import geopandas as gpd
 import pandas as pd
 from kedro.io import AbstractDataset
 
 
 class GeoPackageDataSet(AbstractDataset):
-    def __init__(self, filepath: str, index: str):
+    def __init__(
+            self,
+            filepath: str,
+            index: str = None,
+            driver: Literal['GPKG', 'GeoJSON'] = 'GPKG'):
         self.filepath = filepath
         self.index = index
+        self.driver = driver
 
     def _load(self) -> gpd.GeoDataFrame:
         gdf = gpd.read_file(self.filepath)
@@ -19,7 +26,7 @@ class GeoPackageDataSet(AbstractDataset):
         return gdf
 
     def _save(self, data: gpd.GeoDataFrame) -> None:
-        data.to_file(self.filepath, driver="GPKG")
+        data.to_file(self.filepath, driver=self.driver)
 
     def _describe(self) -> dict:
         return {"filepath": self.filepath}
