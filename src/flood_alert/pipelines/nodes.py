2aed9dcfa9a64352ddc81eb24d2254fc267fdc3e
tadzio <tadzio.holtrop@icloud.com>
2025-04-01 16:22:01 +0100

    add node to check if measurement exceeds flood threshold

--- src/flood_alert/pipelines/nodes.py
+++ src/flood_alert/pipelines/nodes.py
@@ -19,10 +19,13 @@ def set_flood_threshold(data: pd.DataFrame, start_date: str, end_date: str, sigm
     filtered_data = data.sort_index().loc[:, data_range, :]
     # calculate mean and standard deviation of absolute water levels
     data_stats = filtered_data.groupby('station_id')['absolute_water_level_meters'].agg(['mean', 'std'])
+    # add threshold
+    data_stats['threshold'] = data_stats['mean'] + sigma * data_stats['std']
 
-    return data_stats['mean'] + sigma * data_stats['std']
+    return data_stats['threshold']
 
-def exceed_flood_threshold(data: pd.DataFrame, threshold: pd.DataFrame) -> pd.DataFrame:
+
+def exceeds_flood_threshold(data: pd.DataFrame, threshold: pd.DataFrame) -> pd.DataFrame:
     """_summary_
 
     Args:
@@ -32,4 +35,11 @@ def exceed_flood_threshold(data: pd.DataFrame, threshold: pd.DataFrame) -> pd.Da
     Returns:
         pd.DataFrame: _description_
     """
-    
+    merged_data = pd.merge(data, threshold, how='left', left_index=True, right_index=True)
+
+    exceeds_threshold = merged_data.apply(lambda x: x.absolute_water_level_meters > x.threshold,
+                                          axis=1)
+    # set name of pd.Series
+    exceeds_threshold.name = 'exceeds_threshold'
+
+    return exceeds_threshold
